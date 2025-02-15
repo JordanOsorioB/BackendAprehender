fbb5de0a5eeeeccb255872402c9ad00003fab907
Jordán Osorio B. <jordanjesusosoriobustos@gmail.com>
2025-02-14 23:48:04 -0300

    reparos render

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ const userRoutes = require("./routes/userRoutes");
 const courseRoutes = require("./routes/courseRoutes");
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Rutas
 app.use("/users", userRoutes);
 app.use("/courses", courseRoutes);
 
+// Ruta de prueba
 app.get("/", (req, res) => {
   res.send("🚀 API funcionando correctamente.");
 });
 
+// Configuración del puerto para Render
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
