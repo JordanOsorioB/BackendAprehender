a76d0cffb2c7d4113031bf3711e5ff6be3e8165b
Jordán Osorio B. <jordanjesusosoriobustos@gmail.com>
2025-02-14 23:41:25 -0300

    

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
