679a01c383cc18981bad31c5f8a771a2d37acd54
Jordán Osorio Bustos <jordan.osorio_dev@outlook.com>
2025-05-30 23:34:10 -0400

    reparos student, se agregan las rutas faltantes

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -26,6 +26,10 @@ const studentController = require('../controllers/studentController');
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - nombre
+ *               - level
+ *               - experience
  *             properties:
  *               nombre:
  *                 type: string
@@ -36,9 +40,69 @@ const studentController = require('../controllers/studentController');
  *     responses:
  *       200:
  *         description: Estudiante creado
- */
-/**
- * @swagger
+ * /students/{id}:
+ *   get:
+ *     tags: [Students]
+ *     summary: Obtiene un estudiante por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Estudiante encontrado
+ *   put:
+ *     tags: [Students]
+ *     summary: Actualiza un estudiante por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               level:
+ *                 type: integer
+ *               experience:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Estudiante actualizado
+ *   delete:
+ *     tags: [Students]
+ *     summary: Elimina un estudiante por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Estudiante eliminado
+ * /students/{id}/courses:
+ *   get:
+ *     tags: [Students]
+ *     summary: Obtiene los cursos de un estudiante
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Lista de cursos del estudiante
  * /students/{id}/full:
  *   get:
  *     tags: [Students]
@@ -56,6 +120,10 @@ const studentController = require('../controllers/studentController');
 
 router.get('/', studentController.getStudents);
 router.post('/', studentController.createStudent);
+router.get('/:id', studentController.getStudentById);
+router.put('/:id', studentController.updateStudent);
+router.delete('/:id', studentController.deleteStudent);
+router.get('/:id/courses', studentController.getStudentCourses);
 router.get('/:id/full', studentController.getStudentFullData);
 
 module.exports = router;
